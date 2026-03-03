9994ae83f237522d3dd7e439a2d85906b5ab5baf
Jason Barnett <jbarnett2114@gmail.com>
2026-03-03 04:52:46 +0000

    update voices list

--- wyoming-kokoro/server.py
+++ wyoming-kokoro/server.py
@@ -11,12 +11,23 @@ from wyoming.audio import AudioStart, AudioChunk, AudioStop
 from wyoming.tts import Synthesize, SynthesizeStart, SynthesizeChunk, SynthesizeStop, SynthesizeStopped
 
 KOKORO_URL = os.getenv("KOKORO_URL", "http://kokoro:8880/v1/audio/speech")
-DEFAULT_VOICE = os.getenv("KOKORO_VOICE", "af")
+DEFAULT_VOICE = os.getenv("KOKORO_VOICE", "af_heart")
 HOST = os.getenv("WYOMING_HOST", "0.0.0.0")
 PORT = int(os.getenv("WYOMING_PORT", "10200"))
 SAMPLES_PER_CHUNK = int(os.getenv("SAMPLES_PER_CHUNK", "2048"))
 
 print(f"wyoming-kokoro starting on {HOST}:{PORT}, voice={DEFAULT_VOICE}", flush=True)
+
+KOKORO_VOICES = [
+  "af_alloy","af_aoede","af_bella","af_heart","af_jadzia","af_jessica","af_kore","af_nicole","af_nova","af_river",
+  "af_sarah","af_sky","af_v0","af_v0bella","af_v0irulan","af_v0nicole","af_v0sarah","af_v0sky",
+  "am_adam","am_echo","am_eric","am_fenrir","am_liam","am_michael","am_onyx","am_puck","am_santa","am_v0adam",
+  "am_v0gurney","am_v0michael","bf_alice","bf_emma","bf_lily","bf_v0emma","bf_v0isabella","bm_daniel","bm_fable",
+  "bm_george","bm_lewis","bm_v0george","bm_v0lewis","ef_dora","em_alex","em_santa","ff_siwis","hf_alpha","hf_beta",
+  "hm_omega","hm_psi","if_sara","im_nicola","jf_alpha","jf_gongitsune","jf_nezumi","jf_tebukuro","jm_kumo",
+  "pf_dora","pm_alex","pm_santa","zf_xiaobei","zf_xiaoni","zf_xiaoxiao","zf_xiaoyi","zm_yunjian","zm_yunxi",
+  "zm_yunxia","zm_yunyang"
+]
 
 def make_info_event() -> Event:
     return Event(
@@ -26,26 +37,18 @@ def make_info_event() -> Event:
                 {
                     "name": "kokoro",
                     "description": "Kokoro TTS via HTTP bridge",
-                    "attribution": {
-                        "name": "Kokoro",
-                        "url": "https://huggingface.co/hexgrad/Kokoro-82M",
-                    },
+                    "attribution": {"name": "Kokoro", "url": "https://huggingface.co/hexgrad/Kokoro-82M"},
                     "installed": True,
                     "voices": [
                         {
-                            "name": DEFAULT_VOICE,
-                            "description": f"Kokoro voice {DEFAULT_VOICE}",
-                            "attribution": {
-                                "name": "Kokoro",
-                                "url": "https://huggingface.co/hexgrad/Kokoro-82M",
-                            },
+                            "name": v,
+                            "description": v,
+                            "attribution": {"name": "Kokoro", "url": "https://huggingface.co/hexgrad/Kokoro-82M"},
                             "installed": True,
-                            "languages": ["en"],
-                            "speakers": None,
-                            "version": None,
+                            "languages": ["en"],  # you can refine later
                         }
+                        for v in KOKORO_VOICES
                     ],
-                    "version": None,
                 }
             ]
         },
